45ca167e662f5d428e7a9fbec8e38fbbfb5067f3
lmarquar <leon.marquardt@gmx.de>
2026-03-25 16:42:08 +0100

    Revert "a"
    This reverts commit 9930848fc9f98786c2ec274928b76eb5367a9b7f.

--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -23,7 +23,6 @@ const projectSites: Array<{
 	{ key: "calcrow", url: "https://calcrow.com" },
 	{ key: "stimmapp", url: "https://stimmapp.net" },
 	{ key: "portopener", url: "https://portopener.com", note: "unprototyped" },
-	{ key: "hexaclamp", url: "https://hexaclamp.com", note: "unprototyped" },
 	{ key: "trexip", url: "https://trexip.com", note: "unprototyped" },
 ];
 
